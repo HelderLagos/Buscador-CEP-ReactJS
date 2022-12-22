67622e1664a607e8159dacb5839225161621a0da
Helder Lagos <helderlagospro@gmail.com>
2022-12-22 08:03:18 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
+import api from './services/api';
+
+
+
 function App() {
+
+  const [input,setInput] = useState('');
+  const [cep,setCep] = useState('');
+
+  const handleSearch = async () => {
+    //01001000/json/
+
+    if (input === '') {
+      alert("Preencha algum cep!")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      console.log(response.data)
+      setCep(response.data)
+      setInput('')
+
+    }catch{
+      alert("Erro ao buscar. Verifique o que digitou")
+      setInput('')
+
+    }
+
+  }
+
   return (
     <div className="container">
      <h1 className="title">Buscador CEP</h1>
 
-     <div className="containerImput">
+     <div className="containerInput">
       <input
       type="text"
-      placeholder="Digite seu cep..."      
+      placeholder="Digite seu cep..."
+      value={input}
+      onChange={(e)=> setInput(e.target.value)}
       />
 
-      <button className="buttonSearch">
+      <button className="buttonSearch" onClick={handleSearch}>
         <FiSearch size={25}  color="#fff" />
       </button>
      </div>
 
-     <main className="main">
-      <h2>CEP: 323729837987</h2>
 
-      <span>Rua Teste algum</span>
-      <span>Complemento: complemento</span>
-      <span>Vila rosa</span>
-      <span>Cabo frio - RJ</span>
+    {Object.keys(cep).length > 0 && (
+      <main className="main">
+      <h2>CEP: {cep.cep}</h2>
+
+      <span>{cep.logradouro}</span>
+      
+      <span>{cep.bairro}</span>
+      <span>{cep.localidade}-{cep.uf}</span>
      </main>
+
+    )}
+     
     </div>
   );
 }
